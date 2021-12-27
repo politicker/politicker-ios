a214a6adb8919ac57b2f1b10c1f709fb746a7850
Harrison Borges <hborges005@gmail.com>
2021-12-27 12:54:22 -0500

    Initial Commit

---
+++ politicker/politickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  politickerApp.swift
+//  politicker
+//
+//  Created by Harrison Borges on 12/27/21.
+//
+
+import SwiftUI
+
+@main
+struct politickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
