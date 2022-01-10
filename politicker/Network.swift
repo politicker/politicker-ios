babd198cef25339da9f6096340e725abbd6971fa
Harrison Borges <hborges005@gmail.com>
2022-01-09 19:49:13 -0500

    point to production backend

--- politicker/Network.swift
+++ politicker/Network.swift
@@ -10,6 +10,7 @@ import Apollo
 
 class Network {
   static let shared = Network()
-    
-  private(set) lazy var apollo = ApolloClient(url: URL(string: "http://localhost:8001/graphql")!)
+	
+  private(set) lazy var apollo = ApolloClient(url: URL(string: "https://api.politicker-internal.net/graphql")!)
 }
+
