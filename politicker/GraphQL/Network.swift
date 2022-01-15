2a33b2a89077f4b5b6a4831092a520019127aa33
Harrison Borges <hborges005@gmail.com>
2022-01-15 14:06:13 -0500

    read API URL from environment

--- politicker/GraphQL/Network.swift
+++ politicker/GraphQL/Network.swift
@@ -10,8 +10,9 @@ import Apollo
 
 class Network {
   static let shared = Network()
+  let url = ProcessInfo.processInfo.environment["API_URL"]
 
 //"https://api.politicker-internal.net/graphql"
-  private(set) lazy var apollo = ApolloClient(url: URL(string: "http://localhost:8001/graphql")!)
+  private(set) lazy var apollo = ApolloClient(url: URL(string: url!)!)
 }
 
